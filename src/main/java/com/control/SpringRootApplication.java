312561eab83a6b68c41dbbe27934f5ab083f1a4b
satishmarutham <satishmarutham@gmail.com>
2016-09-18 15:39:07 +0530

    SpringBoot

---
+++ src/main/java/com/control/SpringRootApplication.java
@@ -0,0 +1,12 @@
+package com.control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRootApplication.class, args);
+	}
+}
